bc4747a9a941106bfb604e8968e8301b945d2f16
git <fabio.casini@accenture.com>
2019-11-19 23:03:33 +0100

    update headers for CORS

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -17,6 +17,11 @@ public class HelloWorldHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
+        headers.put("Access-Control-Allow-Origin", "*");
+        headers.put("Access-Control-Allow-Credentials", "true");
+        headers.put("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        headers.put("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
         return new GatewayResponse(new JSONObject().put("Output", "Hello World!").toString(), headers, 200);
     }
 }
